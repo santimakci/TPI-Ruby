3f1c044e44db7bba586c1f9844ffc786e22dc07a
Santiago <santimakcimovich@gmail.com>
2021-01-30 18:28:59 -0300

    Login hecho

---
+++ web/app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+
+  skip_before_action :authorized, only: [:new, :create, :welcome]
+  
+  def new
+  end
+  
+  def close
+    reset_session
+    redirect_to '/welcome'
+  end
+
+  def create
+      @user = User.find_by(username: params[:username])
+   if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
+   else
+      redirect_to '/login'
+
+    end
+  end
+
+  def login
+  end
+
+  def welcome
+  end
+
+  def page_requires_login
+  end
+end
