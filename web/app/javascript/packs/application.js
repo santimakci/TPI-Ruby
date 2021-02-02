ae914298ecc495e434561f3dfe38c4273eebd4fc
Santiago <santimakcimovich@gmail.com>
2021-02-02 01:11:18 -0300

    navbar, index avanzados

--- web/app/javascript/packs/application.js
+++ web/app/javascript/packs/application.js
@@ -1,7 +1,16 @@
+//= require rails-ujs
+//= require jquery
 //= require materialize
+//= require turbolinks
+//= require_tree .
+
+$(document).on('turbolinks:load', function() {
+  $('.dropdown-trigger').dropdown();
+});
 
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 
 Rails.start();
 Turbolinks.start();
+
